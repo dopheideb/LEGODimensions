2876b778c42b03522e38c278fa3e6e4d3468b6df
Bart Dopheide <dopheide@fmf.nl>
2024-06-08 20:57:19 +0200

    tagreaderwriter.py can now write characters.

--- python/tagreaderwriter.py
+++ python/tagreaderwriter.py
@@ -46,7 +46,7 @@ if args.list_names:
 
 
 clf = nfc.ContactlessFrontend('usb')
-logging.debug(f"Found NFC device: {clf}")
+logging.debug(f"Found the following NFC device: {clf}")
 
 logging.info("Waiting for a tag.")
 nfctag = clf.connect(rdwr={'on-connect': lambda nfctag: False})
@@ -54,60 +54,120 @@ nfctag = clf.connect(rdwr={'on-connect': lambda nfctag: False})
 logging.info(f"Basic tag information: {nfctag}")
 
 ldtag = legodimensions.Tag(uid=nfctag.identifier)
-password = ldtag.password
-logging.debug(f"ldtag.password={password.hex()}")
-
+## LEGO protects the page in which the the character/vehicle ID is 
+## written. However, if one buys an empty NTAG213, there is no need to 
+## authenticate. Moreover, if an empty NTAG213 is written to contain a 
+## valid LEGO Dimensions character or vehicle, then LEGO Dimensions does 
+## not REQUIRE that the tag is protected against unwanted reads and 
+## writes. So, if one does not protect an NTAG213, it is perfectly fine. 
+## But then authentication fails if tried.
+## 
+## Just try to read page 0x24 (without prior authentication), where the 
+## vehicle/character ID is stored. Only authenticate if needed.
 password_ack = b"\xAA\x55"
-authenticated = nfctag.authenticate(password + password_ack)
+password = ldtag.password
 
-logging.debug(f"Dump ({'' if authenticated else 'UN'}authenticaed):")
+authentication_needed = False
+is_authenticated = None
+try:
+    page_0x28_to_0x2B = nfctag.read(0x24)
+except nfc.tag.tt2.Type2TagCommandError as e:
+    if e.errno != nfc.tag.tt2.INVALID_PAGE_ERROR:
+        raise
+    authentication_needed = True
+    logging.debug(f"ldtag.password={password.hex()}")
+    is_authenticated = nfctag.authenticate(password + password_ack)
+
+logging.debug(f"Dump ({'' if is_authenticated else 'UN'}authenticaed):")
 for line in nfctag.dump():
     logging.debug(line)
 
-if authenticated:
-    page = nfctag.read(0x24)
-    page0x24 = page[0:4]
-    page0x25 = page[4:8]
-    page0x26 = page[8:12]
-    page0x27 = page[12:16]
+if authentication_needed and not is_authenticated:
+    raise f"Authentication is needed, but authentication failed."
+
+page = nfctag.read(0x24)
+page0x24 = page[0:4]
+page0x25 = page[4:8]
+page0x26 = page[8:12]
+page0x27 = page[12:16]
+
+## A vehicle (type 0x00010000) is handled differently from a 
+## character.
+current_tag_type = None
+if page0x26 == b"\x00\x01\x00\x00":
+    current_tag_type = 'token'
+    word = page0x24[0:2]
+    vehicle_id = int.from_bytes(word, byteorder='little')
+    logging.info(f"Tag contains a vehicle/token with id {vehicle_id} ({taglist[str(vehicle_id)]['name']}).")
+elif page0x26 == b"\x00\x00\x00\x00":
+    current_tag_type = 'character'
+    character_doubled = ldtag.decrypt(page0x24 + page0x25)
+    character1 = character_doubled[0:4]
+    character2 = character_doubled[4:8]
+    if character1 != character2:
+        logging.error(f"Decryption error: Character IDs differ ({character1} != ${character2}).")
+    character_id = int.from_bytes(character1, byteorder='big')
+    try:
+        character_name = taglist[str(character_id)]['name']
+        logging.info(f"Character with id {character_id} ({character_name}).")
+    except KeyError:
+        logging.error(f"Character ID {character_id} could not be converted to a name.")
+        if character1 != character2:
+            logging.error(f"Is this an empty tag perhaps?")
+else:
+    logging.error("The current tag is NOT a LEGO Dimensions character nor is a vehicle/token.")
+
+
+
+if args.write:
+    page_0x24 = None
+    page_0x25 = None
+    page_0x26 = None	## The type. 0: character, 1: vehicle/token
     
-    if page0x26 == b"\x00\x01\x00\x00":
-        word = page0x24[0:2]
-        vehicle_id = int.from_bytes(word, byteorder='little')
-        logging.info(f"Vehicle with id {vehicle_id} ({taglist[str(vehicle_id)]['name']}).")
-    else:
-        character_doubled = ldtag.decrypt(page0x24 + page0x25)
-        character1 = character_doubled[0:4]
-        character2 = character_doubled[4:8]
-        assert character1 == character2, "Characters differ"
-        character_id = int.from_bytes(character1, byteorder='big')
-        logging.info(f"Character with id {character_id} ({taglist[str(character_id)]['name']}).")
-    
-    if args.write:
-        tag = taglist[args.write]
+    tag = taglist[args.write]
+    if tag['type'] == 'character':
+        logging.info(f"Converting this tag to ID {tag['id']} ({tag['name']}).")
+        page_0x26 = b"\x00\x00\x00\x00"
         
-        if tag['type'] == 'character':
-            logging.error('No support for writing characters. Sorry.')
-        elif tag['type'] == 'token':
-            logging.info(f"Converting this tag to ID {tag['id']} ({tag['name']}).")
-            logging.info(f"Writing to page 0x2B (the password): {password} ({password.hex()})")
-            nfctag.write(0x2B, password)
-            
-            password_ack_bytes = password_ack + b"\x00\x00"
-            logging.info(f"Writing to page 0x2C (the password ack): {password_ack_bytes} ({password_ack_bytes.hex()})")
-            nfctag.write(0x2C, password_ack_bytes)
-            
-            id_bytes = int.to_bytes(tag['id'], length=4, byteorder='little')
-            logging.info(f"Writing to page 0x24 (the ID): {id_bytes} ({id_bytes.hex()})")
-            nfctag.write(0x24, id_bytes)
-            
-            logging.info(f"Writing to page 0x25: clearing with zeroes")
-            nfctag.write(0x25, b"\x00\x00\x00\x00")
-            
-            ## A genuine LEGO NFC tag is write protected. Such a 
-            ## token/vehicle tag has page 0x26 already set to 
-            ## b"\x00\x01\x00\x00" and can't be overwritten.
-            #type_bytes = b"\x00\x01\x00\x00"
-            #logging.info(f"Writing to page 0x26 (the type): {type_bytes} ({type_bytes.hex()})")
-            #nfctag.write(0x26, b"\x00\x01\x00\x00")
+        encrypted_character = ldtag.encrypt(tag['id'])
+        logging.info(f"Encrypted data to write to pages 0x24 and 0x25: {encrypted_character} ({encrypted_character.hex()})")
+        
+        page_0x24 = encrypted_character[0:4]
+        page_0x25 = encrypted_character[4:8]
+    elif tag['type'] == 'token':
+        logging.info(f"Converting this tag to ID {tag['id']} ({tag['name']}).")
+        type_bytes = b"\x00\x01\x00\x00"
+        
+        ## Page 0x24 contains the vehicle/token ID.
+        page_0x24 = int.to_bytes(tag['id'], length=4, byteorder='little')
+        
+        ## Page 0x25 needs to be zeroed.
+        page_0x25 = b"\x00\x00\x00\x00"
+    
+    ## We cannot switch between vehicle/token with a genuine LEGO 
+    ## Dimensions tag.
+    if tag['type'] != current_tag_type:
+        if authentication_needed:
+            raise ValueError(f"This is assumed to be a genuine LEGO Dimensions tag. We cannot alter the tag type from '{current_tag_type}' to '{tag['type']}'.")
+        logging.info(f"Writing to page 0x26 (the type): {page_0x26} ({page_0x26.hex()})")
+        nfctag.write(0x26, page_0x26)
+    
+    logging.info(f"Writing to page 0x24: {page_0x24} ({page_0x24.hex()})")
+    nfctag.write(0x24, page_0x24)
+    
+    logging.info(f"Writing to page 0x25: {page_0x25} ({page_0x25.hex()})")
+    nfctag.write(0x25, page_0x25)
+    
+    ## Page 0x29 consists of 4 bytes:
+    ##     Byte 0+1: PACK
+    ##     Byte 2:3: RFUI, must be all ones according to the docs: 
+    ##     "Reserved for future use - implemented. Write all bits 
+    ##     and bytes denoted as RFUI as 0b."
+    page_0x2C = password_ack + b"\x00\x00"
+    
+    logging.info(f"Writing to page 0x2B (the password): {password} ({password.hex()})")
+    nfctag.write(0x2B, password)
+    logging.info(f"Writing to page 0x2C (the password ack and RFUI): {page_0x2C} ({page_0x2C.hex()})")
+    nfctag.write(0x2C, page_0x2C)
+
 clf.close()
