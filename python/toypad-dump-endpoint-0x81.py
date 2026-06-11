b5f743c2d01cf79507ff07985b618d11abfaff75
Bart Dopheide <3164970+dopheideb@users.noreply.github.com>
2026-06-11 21:49:46 +0200

    Added GNU GPL 2.0 license.

--- python/toypad-dump-endpoint-0x81.py
+++ python/toypad-dump-endpoint-0x81.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+'''
+Copyright (C) 2026 Bart Dopheide
+
+This program is free software; you can redistribute it and/or modify it 
+under the terms of the GNU General Public License version 2 as published 
+by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License along 
+with this program; if not, see <https://www.gnu.org/licenses/>.
+'''
+
 ## Inspired by:
 ## * https://wasabifan.github.io/ev3dev.github.io/docs/tutorials/using-lego-dimensions-toy-pad/
 ## * https://github.com/woodenphone/lego_dimensions_protocol/blob/master/command%20notes/Special/b0.py
