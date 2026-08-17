1ca35ddf33e73aafe14a6cc1658e0461cc73d8f7
Bart Dopheide <3164970+dopheideb@users.noreply.github.com>
2026-08-17 16:59:32 +0200

    Make it clear that we are using big endian.

--- python/unittest_legodimensions.py
+++ python/unittest_legodimensions.py
@@ -7,7 +7,7 @@ import unittest
 class TestTEA(unittest.TestCase):
 	def test_nfc_access_password(self):
 		uid_int = 0x0413BB1A994080
-		uid_bytes = uid_int.to_bytes(7)
+		uid_bytes = uid_int.to_bytes(7, byteorder='big')
 
 		password_int = 0x4bef3621
 		password_bytes = password_int.to_bytes(4, byteorder='big')
@@ -16,7 +16,7 @@ class TestTEA(unittest.TestCase):
 
 	def test_tea_key(self):
 		uid_int = 0x0413BB1A994080
-		uid_bytes = uid_int.to_bytes(7)
+		uid_bytes = uid_int.to_bytes(7, byteorder='big')
 
 		tea_key_int = 0x33ef82233a56082f78f06c7c246c3710
 		tea_key_bytes = tea_key_int.to_bytes(16, byteorder='big')
