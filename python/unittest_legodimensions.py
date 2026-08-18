2f27b199a375f8065f2d4605a4e5c0d5111dcf8f
Bart Dopheide <3164970+dopheideb@users.noreply.github.com>
2026-08-18 10:10:22 +0200

    1. Use bytes.fromhex() to not bother us with endianness. 2. Added argument parsing, to be verbose or not. 3. Added more tests.

--- python/unittest_legodimensions.py
+++ python/unittest_legodimensions.py
@@ -1,29 +1,52 @@
 #!/usr/bin/env python3
 
+import argparse
 import logging
 from   legodimensions import Tag
 import unittest
 
 class TestTEA(unittest.TestCase):
-	def test_nfc_access_password(self):
-		uid_int = 0x0413BB1A994080
-		uid_bytes = uid_int.to_bytes(7, byteorder='big')
+	def test_nfc_access_password1(self):
+		uid = bytes.fromhex("04 13 BB 1A 99 40 80")
+		password = bytes.fromhex("4b ef 36 21")
+		self.assertEqual(Tag(uid=uid).password, password)
 
-		password_int = 0x4bef3621
-		password_bytes = password_int.to_bytes(4, byteorder='big')
+	def test_nfc_access_password2(self):
+		uid = bytes.fromhex("00 11 22 33 44 55 66")
+		password = bytes.fromhex("94 28 cc fc")
+		self.assertEqual(Tag(uid=uid).password, password)
 
-		self.assertEqual(Tag(uid=uid_bytes).password, password_bytes)
+	def test_nfc_access_password3(self):
+		uid = bytes.fromhex("00 00 00 00 00 00 00")
+		password = bytes.fromhex("0b ac db dd")
+		self.assertEqual(Tag(uid=uid).password, password)
 
-	def test_tea_key(self):
-		uid_int = 0x0413BB1A994080
-		uid_bytes = uid_int.to_bytes(7, byteorder='big')
+	def test_tea_key1(self):
+		uid = bytes.fromhex("04 13 BB 1A 99 40 80")
+		tea_key = bytes.fromhex("33 ef 82 23 3a 56 08 2f 78 f0 6c 7c 24 6c 37 10")
+		self.assertEqual(Tag(uid=uid).tea_key, tea_key)
 
-		tea_key_int = 0x33ef82233a56082f78f06c7c246c3710
-		tea_key_bytes = tea_key_int.to_bytes(16, byteorder='big')
+	def test_tea_key2(self):
+		uid = bytes.fromhex("00 11 22 33 44 55 66")
+		tea_key = bytes.fromhex("3a a9 12 01 87 e6 15 b8 55 ba 87 9c 74 a6 de 65")
+		self.assertEqual(Tag(uid=uid).tea_key, tea_key)
 
-		self.assertEqual(Tag(uid=uid_bytes).tea_key, tea_key_bytes)
+	def test_tea_key3(self):
+		uid = bytes.fromhex("00 00 00 00 00 00 00")
+		tea_key = bytes.fromhex("74 14 98 30 90 0c 02 08 74 8c cc cc 0a fe e5 d6")
+		self.assertEqual(Tag(uid=uid).tea_key, tea_key)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    unittest.main()
+	parser = argparse.ArgumentParser(
+		prog='Unit tester for the module legodimenions',
+		#description='',
+	)
+	parser.add_argument('--verbose', '-v', action='store_true', help='Be verbose during testing.')
+	args = parser.parse_args()
+
+	if args.verbose:
+		logging.basicConfig(level=logging.DEBUG)
+	else:
+		logging.basicConfig(level=logging.INFO)
+	unittest.main()
