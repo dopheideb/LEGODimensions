15aa34427e6521259e302c0940dd51fdeac0df6a
Bart Dopheide <3164970+dopheideb@users.noreply.github.com>
2026-08-18 12:02:20 +0200

    1. Give all UIDs a name, for better readability and maintenance. 2. Loop over tags, instead of separate tests.

--- python/unittest_legodimensions.py
+++ python/unittest_legodimensions.py
@@ -5,36 +5,69 @@ import logging
 from   legodimensions import Tag
 import unittest
 
+tags =\
+{
+	'Wyldstyle':
+	{
+		'NAME': 'Wyldstyle',
+		'IS_CHARACTER': True,
+		'GENUINE': True,
+		'ID': 3,
+		'UID': bytes.fromhex('04:13:BB:1A:99:40:80'.replace(':', '')),
+		'PAGE_24_25': bytes.fromhex('01 39 ED 60   E4 BE 30 7C'),
+		'AUTH_PASSWORD': bytes.fromhex('4b ef 36 21'),
+		'TEA_KEY': bytes.fromhex('33 ef 82 23 3a 56 08 2f 78 f0 6c 7c 24 6c 37 10'),
+	},
+
+	'BMO':
+	{
+		'NAME': 'BMO',
+		'IS_CHARACTER': False,
+		'GENUINE': True,
+		'ID': 1173,
+		'UID': bytes.fromhex('04:D9:C8:DA:A2:40:80'.replace(':', '')),
+		'PAGE_24_25': bytes.fromhex('95 04 00 00   00 00 00 00'),
+		'AUTH_PASSWORD': bytes.fromhex('51 91 01 d0'),
+		'TEA_KEY': bytes.fromhex('66 3a f2 fc 64 0f 96 cb d3 08 9a 58 4d 81 7d 6d'),
+	},
+
+	'Supergirl':
+	{
+		'NAME': 'Supergirl',
+		'IS_CHARACTER': True,
+		'GENUINE': False,
+		'ID': 46,
+		'UID': bytes.fromhex('04:58:E4:52:25:20:91'.replace(':', '')),
+		'PAGE_24_25': bytes.fromhex('4B 63 B1 08   8F 63 8A 8D'),
+		'AUTH_PASSWORD': bytes.fromhex('85 8d 20 9b'),
+		'TEA_KEY': bytes.fromhex('a1 f1 00 40 54 6f 19 29 a9 e6 5e d3 04 3b ad ce'),
+	},
+}
+
 class TestTEA(unittest.TestCase):
 	def test_nfc_access_password1(self):
-		uid = bytes.fromhex("04 13 BB 1A 99 40 80")
-		password = bytes.fromhex("4b ef 36 21")
-		self.assertEqual(Tag(uid=uid).password.hex(':'), password.hex(':'))
-
-	def test_nfc_access_password2(self):
-		uid = bytes.fromhex("00 11 22 33 44 55 66")
-		password = bytes.fromhex("94 28 cc fc")
-		self.assertEqual(Tag(uid=uid).password.hex(':'), password.hex(':'))
-
-	def test_nfc_access_password3(self):
-		uid = bytes.fromhex("00 00 00 00 00 00 00")
-		password = bytes.fromhex("0b ac db dd")
-		self.assertEqual(Tag(uid=uid).password.hex(':'), password.hex(':'))
+		for tag in tags.values():
+			print(tag)
+			uid    = tag['UID']
+			answer = tag['AUTH_PASSWORD']
+			self.assertEqual(Tag(uid=uid).password.hex(':'), answer.hex(':'))
 
 	def test_tea_key1(self):
-		uid = bytes.fromhex("04 13 BB 1A 99 40 80")
-		tea_key = bytes.fromhex("33 ef 82 23 3a 56 08 2f 78 f0 6c 7c 24 6c 37 10")
-		self.assertEqual(Tag(uid=uid).tea_key.hex(':'), tea_key.hex(':'))
+		for tag in tags.values():
+			uid    = tag['UID']
+			answer = tag['TEA_KEY']
+			self.assertEqual(Tag(uid=uid).tea_key.hex(':'),	answer.hex(':'))
 
-	def test_tea_key2(self):
-		uid = bytes.fromhex("00 11 22 33 44 55 66")
-		tea_key = bytes.fromhex("3a a9 12 01 87 e6 15 b8 55 ba 87 9c 74 a6 de 65")
-		self.assertEqual(Tag(uid=uid).tea_key.hex(':'), tea_key.hex(':'))
+	def test_encrypt1(self):
+		for tag in tags.values():
+			if not tag['IS_CHARACTER']:
+				continue
+			id     = tag['ID']
+			uid    = tag['UID']
+			answer = tag['PAGE_24_25']
+			message = Tag(uid=uid).encrypt(lego_dimesions_id=id)
+			self.assertEqual(message.hex(':'), answer.hex(':'))
 
-	def test_tea_key3(self):
-		uid = bytes.fromhex("00 00 00 00 00 00 00")
-		tea_key = bytes.fromhex("74 14 98 30 90 0c 02 08 74 8c cc cc 0a fe e5 d6")
-		self.assertEqual(Tag(uid=uid).tea_key.hex(':'), tea_key.hex(':'))
 
 
 if __name__ == "__main__":
